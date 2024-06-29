d3350d0a82e61f9bdcd7ef38fce0cd56db68dd40
jon4444 <50190069+jon4444@users.noreply.github.com>
2024-06-30 00:13:41 +0100

    added download button

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+            <img src={assets.logo} alt="LOGO" />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            </p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+44-799-995-5533</li>
+                <li>contact@tomato.com</li>
+            </ul>
+            </div>
+        </div>
+        <hr />
+        <p className='footer-copyright'>2024 &copy; Olive Catering - All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
