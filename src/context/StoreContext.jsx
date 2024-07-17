5f9001257078d0c45d5313063b04db569d44ca24
jon4444 <50190069+jon4444@users.noreply.github.com>
2024-07-17 21:46:36 +0100

    another commit

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -21,13 +21,13 @@ const StoreContextProvider = (props) => {
   const getTotalCartAmount = () => {
     let totalAmount = 0;
     for (const item in cartItems) {
-      if (cartItems[item]>0) {
-        let itemInfo = food_list.find((product)=> product._id === item);
-      totalAmount += itemInfo.price* cartItems[item];
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
       }
     }
     return totalAmount;
-  }
+  };
 
   const contextValue = {
     food_list,
@@ -35,7 +35,7 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
-    getTotalCartAmount
+    getTotalCartAmount,
   };
   return (
     <StoreContext.Provider value={contextValue}>
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -23,7 +23,7 @@ const Cart = () => {
         <hr />
         {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
-            return (
+            return ( 
               <div>
                 <div className="cart-items-title cart-items-item">
                   <img src={item.image} alt="" />
